6418149a8d406dc1a9bd6a2a920a33acc75bd03c
Bohdkr <bkryvsha@gmail.com>
2024-02-17 10:48:04 +0100

    SyncFolders Project

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+logger = logging.getLogger(__name__)
 
 
 def copy_file(source, replica):
     with open(source, 'r') as source_file:
         with open(replica, 'w') as replica_file:
             replica_file.write(source_file.read())
+
+    logger.info("Copied file into replica folder")
 
 
 def sync_folders(source, replica):
@@ -16,11 +23,19 @@ def sync_folders(source, replica):
             sync_folders(source_item, replica_item)
         else:
             copy_file(source_item, replica_item)
-            print("Copied")
+
+    logger.info("Created file in replica folder")
 
 
 if __name__ == "__main__":
     source_folder = input("Enter source folder: ")
     replica_folder = input("Enter replica folder: ")
+    logger_file = input("Enter path to logging file: ")
+
+    log_file = logger_file
+    file_handler =logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+    logger.addHandler(file_handler)
 
     sync_folders(source_folder, replica_folder)
